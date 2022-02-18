4b10eccb5ac37b64772a3cb15c943cfd973d5618
Nurhaliza Hassan <nurhalizahassan@Nurhalizas-Air.lan>
2022-02-18 00:18:18 -0800

    implemeted structure for game

--- pong.py
+++ pong.py
@@ -1,1 +1,15 @@
-import turtle
+import turtle # enables graphics for games
+
+pong = turtle.Screen()
+pong.title("Pong Game by Nurhaliza H")
+pong.bgcolor("blue")
+pong.setup(width = 800, height = 600)
+pong.tracer(0) # stops the window from updating - speeds up game process
+
+# paddle (a)
+paddleA = turtle.Turtle()
+paddleA.speed(0) # sets max animation speed
+paddleA.shape("square")
+paddleA.color("grey")
+paddleA.penup()
+# paddle (b)
